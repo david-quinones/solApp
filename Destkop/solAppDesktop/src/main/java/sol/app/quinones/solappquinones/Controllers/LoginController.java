dce5295a7d8f37257b998a15bbdcc32b5bbf8937
dquinones92 <david.quinones@glamsw.com>
2023-10-07 13:58:11 +0200

    Add Login View

--- Destkop/solAppDesktop/src/main/java/sol/app/quinones/solappquinones/Controllers/LoginController.java
+++ Destkop/solAppDesktop/src/main/java/sol/app/quinones/solappquinones/Controllers/LoginController.java
@@ -1,4 +1,12 @@
 package sol.app.quinones.solappquinones.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public Label lbl_usuari;
+    public TextField fld_usuari;
+    public TextField fld_password;
+    public Button btn_accedir;
 }
---
+++ Destkop/solAppDesktop/src/main/java/sol/app/quinones/solappquinones/Controllers/user/UserMenuController.java
@@ -0,0 +1,4 @@
+package sol.app.quinones.solappquinones.Controllers.user;
+
+public class UserMenuController {
+}
