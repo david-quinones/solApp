52d194b373f004f619a7dfdd4ef6d4f75df80851
dquinones92 <david.quinones@glamsw.com>
2023-10-11 22:26:53 +0200

    Log Out
    First implementation only local logout, pendenfing call server and read response

--- Destkop/solAppDesktop/src/main/java/sol/app/quinones/solappquinones/Controllers/admin/AdminMenuController.java
+++ Destkop/solAppDesktop/src/main/java/sol/app/quinones/solappquinones/Controllers/admin/AdminMenuController.java
@@ -2,6 +2,9 @@ package sol.app.quinones.solappquinones.Controllers.admin;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import sol.app.quinones.solappquinones.Models.Model;
+import sol.app.quinones.solappquinones.Service.SingletonConnection;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -21,6 +24,19 @@ public class AdminMenuController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        btn_logout.setOnAction(event -> logout());
+
+    }
+
+    public void logout(){
+        //TODO refactorizar logout, todas lo tienen solo 1 vez escrito
+        //liberar token si dice ok server
+        SingletonConnection.getInstance().closeConnection();
+        //mostrat pantalla TODO Refactorizar metodo de cerrar ya que se utiliza muhco
+
+        Stage stage = (Stage) btn_logout.getScene().getWindow(); //obtenim la finestra del label existent
+        Model.getInstance().getViewFactory().closeStage(stage); //tanquem la finestra
+        Model.getInstance().getViewFactory().showLoginWindow(); //mostrem la finesta nova
     }
 
 }
